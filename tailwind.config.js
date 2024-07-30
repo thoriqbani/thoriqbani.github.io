8e1d7a2bc72539e370803a84c844b710e12fc802
thoriqbani <tjrsgamers@gmail.com>
2024-07-30 15:59:36 +0700

    sa

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './*.html',     // Untuk semua file HTML di root folder
+    './src/**/*.css', 
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+};
